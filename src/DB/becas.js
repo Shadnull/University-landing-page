a3e08280e8b03d7d915f62e6cc257cf37857074b
Kaleb <hekamartinezhe@gmail.com>
2025-11-11 11:01:04 -0700

    add scholarship db and assets and astro page

---
+++ src/DB/becas.js
@@ -0,0 +1,21 @@
+export const becasData = [
+    {
+        id: 1,
+        titulo: "Beca Académica",
+        resumen: "Apoyo por de condonación por promedio",
+        monto: "Hasta 100% de descuento",
+        pdfURL: "/assets/becas/Academica.pdf",
+        imagenFondo: "/assets/images/biblioteca-bg.jpg",
+        imagenFrente: "/assets/images/becas/becaacademica.png",
+        activa: true,
+        // Desglosamos la info fea en un array limpio
+        detalles: [
+            { label: "9.0 - 9.2", value: "40%" },
+            { label: "9.3 - 9.4", value: "50%" },
+            { label: "9.5 - 9.6", value: "60%" },
+            { label: "9.7 - 9.8", value: "80%" },
+            { label: "9.9 - 10", value: "100% (Total)" },
+        ]
+
+    }
+]
